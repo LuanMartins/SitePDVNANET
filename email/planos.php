7f6167ec49ca2bf3f3325b899d0255d9e997b699
Luan Martins <luan_18martins@hotmail.com>
2019-05-02 23:10:09 -0300

    Adicionando arquivos necessários para utilização da biblioteca phpMailer para envio de emails

---
+++ email/planos.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once("../vendor/phpmailer/PHPMailer.php");
+require_once("../vendor/phpmailer/SMTP.php");
+
+
+$email = new PHPMailer\PHPMailer\PHPMailer();
+
+var_dump($email);
+?>
