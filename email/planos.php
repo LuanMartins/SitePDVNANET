7a18bf4f4e9234f00b748561bce39f23854cba41
Luan Martins <luan_18martins@hotmail.com>
2019-05-26 20:39:25 -0300

    adicionando layout de envio de email para todos os forms e alterando funação para limpar campos

--- email/planos.php
+++ email/planos.php
@@ -67,6 +67,10 @@ if($_POST) {
         $email->Username = 'contato.pdvnanet@gmail.com';
         $email->Password = 'solucoes@12';
         $email->Port = 587;
+        $email->isHTML(true);
+        $email->CharSet = 'utf-8';
+
+
 
 // configuraÃ§Ã£o de remetente
         $email->setFrom('contato.pdvnanet@gmail.com');
@@ -77,7 +81,47 @@ if($_POST) {
 
 // mensagem de teste
         $email->Subject = "A Equipe do PDVNANET agradece seu contato " + $_POST['nome'];
-        $email->Body = "testando o envio de mensagens <b>HTML</b>! :)";
+        $email->Body = "<html lang='pt'>
+        <head>
+          <meta charset='UTF-8'>
+            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+            <meta http-equiv='X-UA-Compatible' content='ie=edge'>
+        </head>
+        <body>
+        <div style='background: #f0f0f0; padding: 20px; '>
+                    <div style='background: white; width: 80%; margin: auto; border: 1px solid rgb(210, 210, 210);'>
+                        <div style='height: 70px; background: #429244;'></div>
+                        <div style='height: 20px; background: #00a2ea'></div>
+                        <div style='width: 500px; margin: auto; margin-top: 10px; margin-bottom: 10px;'>
+                            <h1><center>ATENÇÃO TIME COMERCIAL</center></h1>
+                        </div>
+                        <div style='width: 80%; margin: auto; margin-top: 10px; margin-bottom: 10px;'>
+                            <div style='text-align: justify; margin-top: 20px; margin-bottom: 20px; font-family: Arial; font-size: 15px;'>
+                            <h3></h3>
+                                <h4>Data:</h4>
+                                <h4>Horário do planos:</h4>
+                                <h4>Nome: </h4>
+                                <h4>Email: </h4>
+                                <h4>Telefone: </h4>
+                                <h4>Empresa:</h4>
+                                <h4>cargo: </h4>
+                                <h3>Atenciosamente,</h3>
+                                <h3>Soluc1one Web System</h3>
+                                <img src='cid:135' width='300px' height='100px'>
+                                <h3>Atenciosamente,</h3>
+                                <h3>Soluc1one Web System</h3>
+                            </div>
+                        </div>
+                        <div style='background: white;'>
+                            <div style='width: 300px; margin: auto; margin-top: 10px; margin-bottom: 10px;'>
+                            
+                            </div>
+                        </div>
+                    </div>	
+                </div>
+        </body>
+        </html>";
+
 
         $enviado = $email->Send();
 
